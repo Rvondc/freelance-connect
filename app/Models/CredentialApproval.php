1738c272dcbef5cf8df049850568831797b43bc7
Chris Dominic P. Chan <chanchrisdominic@gmail.com>
2024-08-02 20:53:42 +0800

    Added CredentialApproval model

--- app/Models/CredentialApproval.php
+++ app/Models/CredentialApproval.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class CredentialApproval extends Model
 {
     use HasFactory;
+
+    protected $table = 'credential_approvals';
+    protected $fillable = [
+        'front_id_image',
+        'back_id_image',
+        'selfie_image',
+        'status',
+        'user_id',
+    ];
 }
