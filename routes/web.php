dbbb44ef6346a4e54a8a370305a989c8e760301c
Chris Dominic P. Chan <chanchrisdominic@gmail.com>
2024-08-03 12:03:47 +0800

    Added /admin to routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Livewire\Admin\AdminHomePage;
 use App\Livewire\Auth\RegisterPage;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', RegisterPage::class);
+Route::get('/admin', AdminHomePage::class);
