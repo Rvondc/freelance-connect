4eacbe2c9f54a3872894eb181e5085929464a625
Rvondc <rogivon.delacruz@gmail.com>
2024-08-03 11:38:27 +0800

    Add a route to view login page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Livewire\Auth\RegisterPage;
+use App\Livewire\Auth\Login;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', RegisterPage::class);
+Route::get("/", RegisterPage::class);
+Route::get("/login", Login::class); 
