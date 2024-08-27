a7f3c9a28a00129a95d2e964f9a4775f24ab4e58
Chris Dominic P. Chan <chanchrisdominic@gmail.com>
2024-08-27 16:08:11 +0800

    Added client and admin layouts

--- resources/views/components/layouts/client.blade.php
+++ resources/views/components/layouts/client.blade.php
@@ -7,18 +7,29 @@
     <link
         rel="stylesheet"
         href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css" />
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
-    <title>{{ $title ?? 'Admin Page' }}</title>
+    <title>{{ $title ?? 'Page Title' }}</title>
     @vite('resources/css/app.css')
 </head>
 
 <body>
     @livewire('wire-elements-modal')
-    <livewire:layouts.admin.sidebar />
+    <livewire:layouts.client.sidebar />
 
     <div class="ml-[300px]">
         {{ $slot }}
     </div>
+
+    <script type="text/javascript">
+        function dropdown() {
+            document.querySelector("#submenu").classList.toggle("hidden");
+            document.querySelector("#arrow").classList.toggle("rotate-180");
+        }
+        dropdown();
+
+        function openSidebar() {
+            document.querySelector(".sidebar").classList.toggle("hidden");
+        }
+    </script>
 </body>
 
 </html>
